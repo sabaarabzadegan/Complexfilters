32bda8edd48a11f1d4bdfaa0b3cf5dab2a44e75a
saba <sabaarabzadegan@gmail.com>
2019-09-15 15:42:09 +0430

    1. three function for getting validated grammar added with thier global variable -> getGrammar, makeGroupChildren, makeGroupHash function and inputState, validGrammar, groupHash variables 2. suggestedItems deleteed and instead we make suggestion based on cuurent node in makeSuggestedItems function and then call it to render as HTMLElements

--- src/complexfilters.js
+++ src/complexfilters.js
@@ -6,6 +6,79 @@ class ComplexFilters extends React.Component {
     constructor(props) {
         super(props);
 
+        /* Storing the states have passed
+         * as an object with 3 properties: 1.field 2.value 3.key
+         * Type: StackArray
+         * Perspective: [
+         *               {field: "..." ,value: "..." ,key: "..." },
+         *               {field: "..." ,value: "..." ,key: "..." },
+         *               .
+         *               .
+         *               .
+         *              ]
+         */
+        this.inputState = [];
+
+
+        /* Storing validated grammar
+         * in validated grammar: 1. validator function run on it
+         *                       2. instead of group name of children in value of children key they have array of children label
+         * Type: Object
+         * Perspective: {
+         *               key1: {children: [child key 1, child key 2,...],...},
+         *               key2: {children: [child key 1, child key 2,...],...},
+         *               .
+         *               .
+         *               .
+         *              }
+         */
+        this.validGrammar = {};
+
+
+        /* Storing all grouped and not grouped grammar nodes that have *label*
+         * based on their group names (key: groupName, value: [object1, object2 ,...])
+         * if not grouped, storing in assumed group name "_" -> (key: "_", value: [object1, object2 ,...])
+         * Type: Object
+         * Perspective: {
+         *               groupName1: [object1, object2 ,...],
+         *               groupName2: [object1, object2 ,...],
+         *               .
+         *               .
+         *               .
+         *               "_": [object1, object2 ,...]
+         *              }
+         *              //Objects have two property: 1.label 2. key of the node
+         */
+        this.groupHash = null;
+
+        this.settings = {
+            classNames: {
+                containerClass: "complexfilters-container",
+                filterBoxClass: "filter-box",
+                inputBoxClass: "input-box",
+                suggestionsClass: "suggestion-drop",
+                tokenItemClass: "token-item",
+                tokenItemCloseClass: "token-item-close",
+                tempTokenItemClass: "temp-token-item",
+                suggestedItemClass: "suggested-item",
+                suggestedValueClass: "suggested-value",
+                suggestedNodeClass: "node-value",
+                suggestedGroupItemClass: "group-item",
+                suggestedGroupClass: "group-value"
+            },
+            startNode: null,
+            grammar: {},
+            validateGrammar: function (options) {
+                return options.grammar;
+            }
+        };
+
+        //calling this function only once in whole here to set "settings" by props
+        this.setSettings();
+
+        let grammar = this.getGrammar(true);
+
+
         this.state = {
 
             //for storing filter box list(tokens)
@@ -49,52 +122,32 @@ class ComplexFilters extends React.Component {
 
             showSuggestedItems: false,
 
-            //for storing suggestions list
-            suggestedItems: {
-                "_": [],
-                "columns": [
-                    {
-                        label: "Time column",
-                        value: "time"
-                    },
-                    {
-                        label: "Text column",
-                        value: "text"
-                    },
-                    {
-                        label: "Key Value Column",
-                        value: "key-value"
-                    }
-                ]
-            },
+            //Storing the current node of grammar
+            currentNode : grammar[this.settings.startNode],
+
+            // //for storing suggestions list
+            // suggestedItems: {
+            //     "_": [],
+            //     "columns": [
+            //         {
+            //             label: "Time column",
+            //             value: "time"
+            //         },
+            //         {
+            //             label: "Text column",
+            //             value: "text"
+            //         },
+            //         {
+            //             label: "Key Value Column",
+            //             value: "key-value"
+            //         }
+            //     ]
+            // },
         };
 
-        this.settings = {
-            classNames: {
-                containerClass: "complexfilters-container",
-                filterBoxClass: "filter-box",
-                inputBoxClass: "input-box",
-                suggestionsClass: "suggestion-drop",
-                tokenItemClass: "token-item",
-                tokenItemCloseClass: "token-item-close",
-                tempTokenItemClass: "temp-token-item",
-                suggestedItemClass: "suggested-item",
-                suggestedValueClass: "suggested-value",
-                suggestedNodeClass: "node-value",
-                suggestedGroupItemClass: "group-item",
-                suggestedGroupClass: "group-value"
-            },
-            startNode: null,
-            grammar: {},
-            validateGrammar: function (options) {
-                return options.grammar;
-            }
-        };
-
-        //calling this function only once in whole here to set "settings" by props
-        this.setSettings();
 
     }
+
 
     /*
      * it does two job:
@@ -136,6 +189,107 @@ class ComplexFilters extends React.Component {
     };
 
     /*
+    * we have following steps here if we have force:
+    * 1. create an options object that has two property: 1. grammar(state grammar)
+    *                                                    2. inputState(global "inputState" contain followed states)
+    * 2. validate new options by calling it's "validateGrammar" function "this.settings" and send it as parameter
+    * 3. calls "makeGroupHash" function for validated grammar returns from previous step to grouping grammar nodes
+    * 4. calls "makeGroupChildren" function for validated grammar to specify it's children
+    * 5. make this grammar as "validGrammar"
+    * parameters: force(Boolean) -> if true validate grammar by mentioned steps else return global "validGrammar"
+    * return: sets global "validGrammar" variable
+    */
+    getGrammar = (force=false) =>{
+        if (force) {
+            const options = {
+                grammar: this.settings.grammar,
+                inputState: this.inputState
+            };
+            let validatedGrammar = this.settings.validateGrammar(options);
+            this.makeGroupHash(validatedGrammar);
+            validatedGrammar = this.makeGroupChildren(validatedGrammar);
+            this.validGrammar = validatedGrammar;
+        }
+        return this.validGrammar;
+    };
+
+    /* grouping grammar nodes that have *label* based on their group names
+     * parameters: validatedGrammar(Object) -> grammar that Already validated
+     * return: sets global "groupHash" variable
+     */
+    makeGroupHash = (validatedGrammar) =>{
+        // "_" group name for ungrouped nodes
+        this.groupHash = {
+            "_": []
+        };
+
+        //Iterate over the nodes in validated grammar
+        for (let keyNode in validatedGrammar) {
+            let grammarNode = validatedGrammar[keyNode];
+            let grammarNodeLabel = getHybrid(grammarNode.label);
+
+            //Create an object contain two property for storing node in their group
+            let groupItem = {
+                label: grammarNodeLabel,
+                value: keyNode
+            };
+
+            /* if node have label then:
+             *     1. if node doesn't have group -> put it in "_" group
+             *     2. else:
+             *          1. if "groupHash" has node group name ->  put it in it's group
+             *          2. else -> create new group named node group name and put it in it's group
+             */
+            if (grammarNodeLabel) {
+                if (grammarNode.group === undefined) {
+                    this.groupHash["_"].push(groupItem);
+                } else {
+                    if (grammarNode.group in this.groupHash) {
+                        this.groupHash[grammarNode.group].push(groupItem);
+                    } else {
+                        this.groupHash[grammarNode.group] = [groupItem];
+                    }
+                }
+            }
+        }
+    };
+
+    /* Replace the group name of children in *children* property of grammar nodes with their own names
+     * parameters: validatedGrammar(Object) -> grammar that validator function run on it
+     * return: validatedGrammar(Object) -> validated grammar with changes on it's nodes children property
+     */
+    makeGroupChildren = (validatedGrammar) =>{
+        /* Iterate over the nodes in "validatedGrammar" then
+         * iterate over the child node of main node then:
+         *     1. if child node specified with it's own name -> leave it
+         *     2. else if child node specifies the group of nodes ->
+         *          find the group name in global "groupHash" and replace group name in main node with
+         *          it's element(grammar nodes of same group) in global "groupHash" variable
+         */
+        for (let keyNode in validatedGrammar) {
+            var grammarNode = validatedGrammar[keyNode];
+            var realChildren = [];
+            for (let child of grammarNode.children) {
+                //Dont know child[1]?????????
+                if (child[0] === '$' && child[1] !== '$') {
+                    if (child.substr(1) in this.groupHash) {
+                        let groupFields = this.groupHash[child.substr(1)];
+                        for (let field of groupFields) {
+                            realChildren.push(field.value);
+                        }
+                    }
+                } else {
+                    realChildren.push(child);
+                }
+            }
+            validatedGrammar[keyNode]["children"] = realChildren;
+        }
+
+        //Return changed validated grammar
+        return validatedGrammar
+    };
+
+    /*
      * handling events of inputWidget HTMLElement
      * Parameters: event
      * Events handle: focus, blur
@@ -168,29 +322,29 @@ class ComplexFilters extends React.Component {
         // event.target.parentElement.remove();
         let tokenItems = this.state.tokenItems.slice();
         tokenItems.splice(event.target.getAttribute("data-key"), 1);
-        // console.log(tokenItems);
         this.setState({tokenItems: tokenItems })
     };
 
     suggestionsEventHandler = (event) => {
         event.preventDefault();
-        alert("pop Up");
+
+        let grammar = this.getGrammar();
+        this.setState({currentNode : grammar[event.target.getAttribute("value")]});
     };
 
-    // rendering "tokenItem" as HTMLElement
+    // rendering "tokenItems" as HTMLElement
     renderTokenItems = () => {
 
-        //next we can use getGrammar function
-        let grammar = this.settings.grammar;
+        let grammar = this.getGrammar();
 
         return this.state.tokenItems.map((token, indexList) => {
             let outValues = token.map((outValue, indexSpans) => {
                 let outValueLabel;
-                for (let key in grammar) {
+                for (let keyNode in grammar) {
                     if (outValue.key === 'value')
                         outValueLabel = outValue.value;
-                    else if (outValue.key === key)
-                        outValueLabel = grammar[key].label;
+                    else if (outValue.key === keyNode)
+                        outValueLabel = grammar[keyNode].label;
                 }
                 // if (outValueLabel !== undefined)
                 return <span key={indexList + "." + indexSpans}>{outValueLabel}</span>;
@@ -215,12 +369,61 @@ class ComplexFilters extends React.Component {
 
     };
 
+    renderInputWidget = () => {
+
+        let inputWidgetType = this.state.currentNode.widget;
+        if (inputWidgetType === "textWidget")
+            inputWidgetType = "text";
+        else if (inputWidgetType === "timeWidget")
+            inputWidgetType = "time";
+        return (
+            <input
+                type={inputWidgetType}
+                onFocus={this.inputWidgetEventHandler}
+                onBlur={this.inputWidgetEventHandler}/>
+        );
+    };
+
+    makeSuggestedItems = () => {
+
+        let grammar = this.getGrammar();
+
+        // "_" group name for ungrouped node
+        let suggestedItemList = {
+            "_": []
+        };
+        for (let child of this.state.currentNode.children) {
+            var grammarNode = grammar[child];
+            var grammarNodeLabel = getHybrid(grammarNode.label);
+            if (grammarNodeLabel) {
+                let groupItem = {
+                    label: grammarNodeLabel,
+                    value: child
+                };
+
+                if (grammarNode.group === undefined) {
+                    suggestedItemList["_"].push(groupItem);
+                } else {
+                    if (grammarNode.group in suggestedItemList) {
+                        suggestedItemList[grammarNode.group].push(groupItem);
+                    } else {
+                        suggestedItemList[grammarNode.group] = [groupItem];
+                    }
+                }
+            }
+        }
+
+        return suggestedItemList;
+    };
+
     // rendering "suggestedItem" as HTMLElement
     renderSuggestedItems = () => {
 
+        let suggestedItemList = this.makeSuggestedItems();
+
         let suggestedItems = [];
         if (this.state.showSuggestedItems) {
-            for (let groupName in this.state.suggestedItems) {
+            for (let groupName in suggestedItemList) {
                 if (groupName !== "_") {
                     suggestedItems.push(
                         <li key={groupName}
@@ -231,7 +434,7 @@ class ComplexFilters extends React.Component {
                         </li>
                     );
                 }
-                suggestedItems.push(this.state.suggestedItems[groupName].map((groupChild) => {
+                suggestedItems.push(suggestedItemList[groupName].map((groupChild) => {
                         let className = this.settings.classNames.suggestedItemClass +
                             " " + this.settings.classNames.suggestedNodeClass;
 
@@ -241,7 +444,6 @@ class ComplexFilters extends React.Component {
 
                         return (
                             <li key={groupName + " " + groupChild.value}
-                                // ? does need now?
                                 value={groupChild.value}
                                 className={className}
                                 onMouseDown={this.suggestionsEventHandler}
@@ -266,10 +468,7 @@ class ComplexFilters extends React.Component {
                     {/*can be written with ListItem component*/}
                     <li className={this.settings.classNames.inputBoxClass}>
                         {/*for storing main input widget*/}
-                        <input
-                            type="text"
-                            onFocus={this.inputWidgetEventHandler}
-                            onBlur={this.inputWidgetEventHandler}/>
+                        {this.renderInputWidget()}
                     </li>
                 </ul>
                 <ul className={this.settings.classNames.suggestionsClass}>
@@ -279,6 +478,13 @@ class ComplexFilters extends React.Component {
         );
     }
 
+}
+
+function getHybrid(obj, value) {
+    if (obj instanceof RegExp) {
+        return obj.test(value);
+    }
+    return (typeof obj == "function")? obj(value): obj;
 }
 
 export default ComplexFilters;
